18b3eb2b1e053da1f078fd3d0ba36ab04618594d
DarkFlamingo <maksym.gavatuk@binary-studio.com>
2021-11-20 12:37:05 +0200

    init Model Finaly

--- server/src/data/models/index.js
+++ server/src/data/models/index.js
@@ -1,11 +1,49 @@
 import { sequelize as orm } from '../db/connection';
 import associate from '../db/associations';
 import { init as initUserModel } from './user/user.model';
+import { init as initAuthoritiesModel } from './authorization/authorization.model';
+import { init as initBlankModel } from './blank/blank.model';
+import { init as initCodeModel } from './code/code.model';
+import { init as initExtractModel } from './extract/extract.model';
+import { init as initLogModel } from './log/log.model';
+import { init as initOldValueModel } from './old_value/old_value.model';
+import { init as initOrganisationModel } from './organisation/organisation.model';
+import { init as initPassportModel } from './passport/passport.model';
+import { init as initPositionModel } from './position/position.model';
 
 const User = initUserModel(orm);
+const Authorities = initAuthoritiesModel(orm);
+const Blank = initBlankModel(orm);
+const Code = initCodeModel(orm);
+const Extract = initExtractModel(orm);
+const Log = initLogModel(orm);
+const OldValue = initOldValueModel(orm);
+const Organisation = initOrganisationModel(orm);
+const Passport = initPassportModel(orm);
+const Position = initPositionModel(orm);
 
 associate({
-  User
+  User,
+  Authorities,
+  Blank,
+  Code,
+  Extract,
+  Log,
+  OldValue,
+  Organisation,
+  Passport,
+  Position
 });
 
-export { User as UserModel };
+export {
+  User as UserModel,
+  Authorities as AuthoritiesModel,
+  Blank as BlankModel,
+  Code as CodeModel,
+  Extract as ExtractModel,
+  Log as LogModel,
+  OldValue as OldValueModel,
+  Organisation as OrganisationModel,
+  Passport as PassportModel,
+  Position as PositionModel
+};
