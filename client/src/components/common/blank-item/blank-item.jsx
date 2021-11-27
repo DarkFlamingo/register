7dd2311cfab70c663e87be3a55bc4e5e8afb5340
DarkFlamingo <maksym.gavatuk@binary-studio.com>
2021-11-27 22:36:52 +0200

    update blank-item

--- client/src/components/common/blank-item/blank-item.jsx
+++ client/src/components/common/blank-item/blank-item.jsx
@@ -2,47 +2,33 @@
 import * as React from 'react';
 import moment from 'moment';
 import { blankType } from 'src/common/prop-types/prop-types';
+import './styles.scss';
 
 const BlankItem = ({ blank }) => (
-  // <List.Item>
-  //   <List.Content>
-  //     <List.Header>{`Серія: ${blank.series}`}</List.Header>
-  //     <List.Description>{`Номер: ${blank.number}`}</List.Description>
-  //     {blank.code ? (
-  //       <>
-  //         <List.Description>{`Код: ${blank.code.code}`}</List.Description>
-  //         <List.Description>{`Номер: ${blank.code.name}`}</List.Description>
-  //       </>
-  //     ) : (
-  //       <List.Description>{`Бланк не витрачений`}</List.Description>
-  //     )}
-  //     <List.Description>{`Дата створення: ${moment(blank.issueDate).format(
-  //       'DD / MM / YY, h:mm:ss'
-  //     )}`}</List.Description>
-  //   </List.Content>
-  // </List.Item>
-  <table>
-    <tr>
-      <th>Серія</th>
-      <th>Номер</th>
-      <th>Код</th>
-      <th>Причина</th>
-      <th>Дата витрачання</th>
-    </tr>
-    <tr>
-      <td>{blank.series}</td>
-      <td>{blank.number}</td>
-      {blank.code ? (
-        <>
-          <td>{blank.code.code}</td>
-          <td>{blank.code.name}</td>
-          <td>{blank.issueDate}</td>
-        </>
-      ) : (
-        <td>Бланк не витрачений</td>
-      )}
-    </tr>
-  </table>
+  <div className={'blank-item'}>
+    <table>
+      <tr>
+        <th>Серія</th>
+        <th>Номер</th>
+        <th>Код</th>
+        <th>Причина</th>
+        <th>Дата витрачання</th>
+      </tr>
+      <tr>
+        <td>{blank.series}</td>
+        <td>{blank.number}</td>
+        {blank.code ? (
+          <>
+            <td>{blank.code.code}</td>
+            <td>{blank.code.name}</td>
+            <td>{blank.issueDate}</td>
+          </>
+        ) : (
+          <td>Бланк не витрачений</td>
+        )}
+      </tr>
+    </table>
+  </div>
 );
 
 BlankItem.propTypes = {
