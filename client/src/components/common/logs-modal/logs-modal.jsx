f7c50cf408b0c5cbc2fb3d5ca54fe2d2b9c7e227
DarkFlamingo <maksym.gavatuk@binary-studio.com>
2021-12-02 21:16:57 +0200

    Add filtering in logs

--- client/src/components/common/logs-modal/logs-modal.jsx
+++ client/src/components/common/logs-modal/logs-modal.jsx
@@ -1,17 +1,76 @@
 /* eslint-disable */
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import { useSelector } from 'react-redux';
-import { Button } from 'src/components/common/common';
+import { Button, Form } from 'src/components/common/common';
+import { log } from 'src/services/services';
 import './styles.scss';
 
 const LogsModal = ({ onClose }) => {
-  const { logs } = useSelector(state => ({
-    logs: state.log.logs
-  }));
+  const [logs, setLogs] = React.useState([]);
+  const [filter, setFilter] = React.useState({
+    name: '',
+    surname: '',
+    series: '',
+    number: null,
+    code: null,
+    codeName: ''
+  });
+
+  React.useEffect(async () => {
+    const updatedLogs = await log.getAllLogs();
+    setLogs(updatedLogs);
+  }, []);
+
+  React.useEffect(async () => {
+    const updatedLogs = await log.getAllLogs();
+
+    const filteredArray = updatedLogs.filter(log => {
+      const nameFiltered = filter.name
+        ? log.user.passport.name.includes(filter.name)
+        : true;
+
+      const surnameFiltered = filter.surname
+        ? log.user.passport.surname.includes(filter.surname)
+        : true;
+
+      const seriesFiltered = filter.series
+        ? log.blank.series.includes(filter.series)
+        : true;
+
+      const codeNameFiltered = !filter.codeName
+        ? true
+        : log.blank.code
+        ? log.blank.code.name.includes(filter.codeName)
+        : false;
+
+      const numberFiltered = filter.number
+        ? log.blank.number.toString().includes(filter.number)
+        : true;
+
+      const codeFiltered = !filter.code
+        ? true
+        : log.blank.code
+        ? log.blank.code.code.toString().includes(filter.code)
+        : false;
+
+      return (
+        nameFiltered &&
+        surnameFiltered &&
+        seriesFiltered &&
+        codeNameFiltered &&
+        numberFiltered &&
+        codeFiltered
+      );
+    });
+    setLogs(filteredArray);
+  }, [filter]);
+
+  const changeFilter = (key, value) => {
+    setFilter({ ...filter, [key]: value });
+  };
 
   return (
-    <div className={'modal-custom'}>
+    <div className={'modal-custom-logs'}>
       <div className={'modal-content'}>
         <table div className={'extract-table'}>
           <tr>
@@ -20,8 +79,63 @@ const LogsModal = ({ onClose }) => {
             <th>Імя реєстратора</th>
             <th>Призвіще</th>
             <th>Серія бланку</th>
+            <th>Номер бланку</th>
             <th>Код витрачання</th>
             <th>Причина витрачання</th>
+          </tr>
+          <tr>
+            <th></th>
+            <th></th>
+            <th>
+              <Form.Input
+                fluid
+                placeholder="Імя"
+                onChange={ev => changeFilter('name', ev.target.value)}
+                value={filter.name}
+              />
+            </th>
+            <th>
+              <Form.Input
+                fluid
+                placeholder="Прізвище"
+                onChange={ev => changeFilter('surname', ev.target.value)}
+                value={filter.surname}
+              />
+            </th>
+            <th>
+              <Form.Input
+                fluid
+                placeholder="Серія"
+                onChange={ev => changeFilter('series', ev.target.value)}
+                value={filter.series}
+              />
+            </th>
+            <th>
+              <Form.Input
+                fluid
+                type="number"
+                placeholder="Номер"
+                onChange={ev => changeFilter('number', ev.target.value)}
+                value={filter.number}
+              />
+            </th>
+            <th>
+              <Form.Input
+                fluid
+                type="number"
+                placeholder="Код"
+                onChange={ev => changeFilter('code', ev.target.value)}
+                value={filter.code}
+              />
+            </th>
+            <th>
+              <Form.Input
+                fluid
+                placeholder="Причина"
+                onChange={ev => changeFilter('codeName', ev.target.value)}
+                value={filter.codeName}
+              />
+            </th>
           </tr>
           {logs.map(el => (
             <tr key={el.id}>
@@ -30,6 +144,7 @@ const LogsModal = ({ onClose }) => {
               <td>{el.user.passport.name}</td>
               <td>{el.user.passport.surname}</td>
               <td>{el.blank.series}</td>
+              <td>{el.blank.number}</td>
               {el.blank.code && (
                 <>
                   <td>{el.blank.code.code}</td>
